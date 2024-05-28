10d66410f7b93fc12437a65122adf98494b22fd9
9ine <ejidoko75@gmail.com>
2024-05-28 14:50:10 +0100

    login page, added with storing the access token using shared_pref and handled endpoint request using http package

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+//ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MemoAuthApp());
+  runApp(const LoginScreen());
 }
 
-class MemoAuthApp extends StatelessWidget {
-  const MemoAuthApp({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +22,7 @@ class MemoAuthApp extends StatelessWidget {
   }
 }
 
+//for the splashscreen
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -24,21 +30,36 @@ class SplashScreen extends StatefulWidget {
   SplashScreenState createState() => SplashScreenState();
 }
 
+//initializes the splah screen before the login page
 class SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _navigateToHome();
+    _navigateToNextScreen();
   }
 
-  _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 3), () {});
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const FingerprintAuthPage()),
-    );
+  _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('accessToken');
+    if (token != null && token.isNotEmpty) {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const FingerprintAuthPage()),
+        );
+      }
+    } else {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreenPage()),
+        );
+      }
+    }
   }
 
+//the first screen you see (spscreen) before the login screen
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
@@ -64,6 +85,194 @@ class SplashScreenState extends State<SplashScreen> {
   }
 }
 
+class LoginScreenPage extends StatefulWidget {
+  const LoginScreenPage({super.key});
+
+  @override
+  State<LoginScreenPage> createState() => _LoginScreenPageState();
+}
+
+//declaration of variables for the email and password
+class _LoginScreenPageState extends State<LoginScreenPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+  bool _isLoading = false;
+
+  Future<void> login() async {
+    final String username = _emailController.text;
+    final String password = _passwordController.text;
+
+    if (username.isEmpty || password.isEmpty) {
+      _showErrorDialog('Email and Password cannot be empty');
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+//this handles the request from the login endpoint
+    var client = http.Client();
+    try {
+      var response = await client.post(
+        Uri.parse(
+            'https://kdsg-authenticator-43d1272b8d77.herokuapp.com/api/auth/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'password': password}),
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseBody = jsonDecode(response.body);
+        if (responseBody['data'] != null &&
+            responseBody['data']['accessToken'] != null) {
+          String accessToken = responseBody['data']['accessToken'] as String;
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString('accessToken', accessToken);
+          if (mounted) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const FingerprintAuthPage()),
+            );
+          }
+        } else {
+          _showErrorDialog('Login failed. Please check your credentials.');
+        }
+      } else {
+        _showErrorDialog('Login failed. Please try again later.');
+      }
+    } catch (e) {
+      _showErrorDialog('An error occurred. Please try again later. Error: $e');
+    } finally {
+      client.close();
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+//displays whatever errors encountered
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+//login page for the app
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.transparent,
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 32.0),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  obscureText: !_isPasswordVisible,
+                ),
+                const SizedBox(height: 32.0),
+                _isLoading
+                    ? const CircularProgressIndicator()
+                    : SizedBox(
+                        width: double.maxFinite,
+                        child: ElevatedButton(
+                          onPressed: login,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.teal,
+                            padding: const EdgeInsets.symmetric(vertical: 15.0),
+                            textStyle: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//biometric page screen
 class FingerprintAuthPage extends StatefulWidget {
   const FingerprintAuthPage({super.key});
 
@@ -79,7 +288,6 @@ class FingerprintAuthPageState extends State<FingerprintAuthPage> {
   Future<void> _authenticate() async {
     bool authenticated = false;
     try {
-      // Check if biometrics are available and any biometric method is enrolled
       bool canCheckBiometrics = await auth.canCheckBiometrics;
       bool isBiometricSupported = await auth.isDeviceSupported();
       List<BiometricType> availableBiometrics =
@@ -94,7 +302,6 @@ class FingerprintAuthPageState extends State<FingerprintAuthPage> {
         return;
       }
 
-      // Set localized reason based on available biometrics
       if (availableBiometrics.contains(BiometricType.face)) {
         _localizedReason = 'Scan your faceID to authenticate';
       } else if (availableBiometrics.contains(BiometricType.fingerprint)) {
@@ -119,6 +326,7 @@ class FingerprintAuthPageState extends State<FingerprintAuthPage> {
     }
   }
 
+//background color for the biometric page screen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
